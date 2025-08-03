a93654913ffb8291f3c3bade8979d0d24144cdc7
Vitaliysmorodin1983 <smorodin38@gmail.com>
2025-08-03 15:57:26 +0700

    Update lesson_2_task_2.py

--- lesson_2_task_2.py
+++ lesson_2_task_2.py
@@ -1,9 +1,12 @@
-year = int(input("Введите год: "))
 def is_year_leap(year):
     if year % 4 == 0:
         return True
     else:
         return False
-    2023
+
+
+year = int(input("Введите год: "))
 result = is_year_leap(year)
+
+
 print(f"год {year}: {result}")
