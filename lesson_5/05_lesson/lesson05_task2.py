aa432ba6686045ff9dd2b34a067e079b52e5b067
Vitaliysmorodin1983 <smorodin38@gmail.com>
2025-08-13 19:07:23 +0700

    https://github.com/Vitaliysmorodin1983/PYTHON_SMORODIN.git

---
+++ lesson_5/05_lesson/lesson05_task2.py
@@ -0,0 +1,30 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from webdriver_manager.chrome import ChromeDriverManager
+
+def click_dynamic_button():
+   
+    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+    
+    try:
+        
+        driver.get("http://uitestingplayground.com/dynamicid")
+        
+        
+        blue_button = WebDriverWait(driver, 5).until(
+            EC.element_to_be_clickable((By.XPATH, "//button[contains(@class, 'btn-primary')]"))
+        )
+        blue_button.click()
+        print("Успешно: кнопка нажата")
+        
+    except Exception as e:
+        print(f"Ошибка: {e}")
+    finally:
+        
+        driver.quit()
+
+if __name__ == "__main__":
+    click_dynamic_button()
