f93115350e676b4a197e7d9fbe51ed8b715b9c50
Vitaliysmorodin1983 <smorodin38@gmail.com>
2025-08-14 07:04:04 +0700

    https://github.com/Vitaliysmorodin1983/PYTHON_SMORODIN.git

--- lesson_5/lesson05_task1.py
+++ lesson_5/lesson05_task1.py
@@ -3,27 +3,27 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
 
+
 def click_blue_button():
-    
+
     driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
-    
+
     try:
-        
+
         driver.get("http://uitestingplayground.com/classattr")
-        
-        
-        blue_button = driver.find_element(By.XPATH, "//button[contains(concat(' ', @class, ' '), ' btn-primary ')]")
+
+        blue_button = driver.find_element(
+            By.XPATH, "//button[contains(concat(' ', @class, ' '), ' btn-primary ')]")
         blue_button.click()
-        
+
         driver.implicitly_wait(2)
-        
-        
-        
+
     except Exception as e:
         print(f"Произошла ошибка: {e}")
     finally:
-        
+
         driver.quit()
+
 
 if __name__ == "__main__":
     click_blue_button()
--- lesson_5/lesson05_task3.py
+++ lesson_5/lesson05_task3.py
@@ -3,35 +3,32 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.firefox.service import Service
 from webdriver_manager.firefox import GeckoDriverManager
 
+
 def input_operations():
-    
+
     driver = webdriver.Firefox(service=Service(GeckoDriverManager().install()))
-    
+
     try:
-        
+
         driver.get("http://the-internet.herokuapp.com/inputs")
-        
-        
+
         input_field = driver.find_element(By.TAG_NAME, "input")
-        
-        
+
         input_field.send_keys("Sky")
-        
-       
+
         input_field.clear()
-        
-        
+
         input_field.send_keys("Pro")
-        
-        
+
         import time
         time.sleep(4)
-        
+
     except Exception as e:
         print(f"Произошла ошибка: {e}")
     finally:
-        
+
         driver.quit()
+
 
 if __name__ == "__main__":
     input_operations()
